91591261a7caa92024ac298631002b824dfb4535
Taviin7 <otaviorl711@gmail.com>
2023-06-06 20:43:14 -0300

    Atualizando o footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import "./Footer.css";
 import Logo from "./proveitLogo.png";
+import getIN from "./getInGooglePlay.jpg";
 
 export default function Footer() {
   return (
-    <section className="footer">
+    <footer>
       <div className="container-footer">
         <div>
           <img src={Logo} />
         </div>
         <div className="boxFooter">
-          <text>Nosso Objetivo</text>
-          <text>Como Usar</text>
-          <text>Funcinalidades</text>
-          <text>Quem Somos</text>
+          <a href="#nosso-objetivo">Nosso Objetivo</a>
+          <a href="#funcionalidades">Funcionalidades</a>
+          <a href="#como-usar">Como Usar</a>
+          <a href="#quem-somos">Quem Somos</a>
+        </div>
+        <div className="download">
+          <img src={getIN} />
         </div>
       </div>
-    </section>
+      <div className="copyright">
+        <span>PROVEIT Copyright © 2023 ProveIT - All rights reserved</span>
+      </div>
+    </footer>
   );
 }
