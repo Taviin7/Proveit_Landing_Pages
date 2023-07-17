6ff80102fece1c572d9e389622a8d787d07a6192
romagrinho <romariosaguilar@gmail.com>
2023-07-17 16:06:09 -0300

    tela "objetivo" 101% reformulada

--- src/Components/Objetivo/Objetivo.jsx
+++ src/Components/Objetivo/Objetivo.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Objetivo.css";
 import Salada from "./imgs/saladafoda.png";
+import Pontos from "./imgs/pontos.png";
 
 export default function Objetivo() {
   return (
     <section className="section2" id="nosso-objetivo">
       <div className="titleBox">
         <div className="titleBox2">
-        <h1 className="fontBlack">Qual o nosso</h1>
-        <h1 className="font-Orange2">objetivo?</h1>
+          <h1 className="fontBlack">Qual o nosso</h1>
+          <h1 className="font-Orange2">objetivo?</h1>
         </div>
         <text className="font-Text">
           Nosso objetivo por meio do nosso aplicativo é conseguir facilitar a
@@ -16,6 +17,9 @@ export default function Objetivo() {
           principais ferramentas.
         </text>
       </div>
+ {/*      <div className="imgPontos">
+        <img src={Pontos} />
+      </div> */}
       <div className="imgOrange">
         <img src={Salada} />
       </div>
